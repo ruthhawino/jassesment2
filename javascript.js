4f921db92e3f71f9ac81209283383b106579dda9
ruthhawino <awinoruthh@gmail.com>
2023-04-11 09:04:21 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,69 @@
+//Write a function that takes in a string and returns it when reversed
+
+let food = "eating"
+ 
+console.log (food.reverse);
+
+//Write a function that takes in the following array and consoles the target if it is found else
+null
+let num = [2,8,0,23,5,45,76]
+Target = 23
+function devidedArray(index,target){
+    if (num.length <= -1){
+        return arr
+    }
+
+    let middleIndex = Math.floor((lettIndex + rigthIndex)/2)
+    let rigthIndex= num.slice(middle)
+    let leftIndex = (0,middle)
+    return(devidedArray(leftIndex),devidedArray(rigthIndex))
+
+}
+
+function sortedArray(rigthIndex,leftIndex){
+    if (num.length <=-1){
+        return num
+    }
+    let leftIndex  =(0,middleIndex)
+    let rigthIndex = (middleIndex)
+    let middleIndex =((leftIndex+rigthIndex)/2)
+    
+
+}
+
+
+
+
+
+
+ function binarysearch(target,ndex){
+    if (num2.length <=-1){
+        return num2
+    } 
+    let leftIndex = num2.length(0)
+    let rigthIndex = num2.length(-1)
+    let middleIndex =((leftIndex + rigthIndex)/2)
+    while(leftIndex<=1){
+        if(rigthIndex[0]<=leftIndex[0]){
+            lettIndex = middleIndex +1
+        }
+        else if (rigthIndex[0]>=leftIndex[0]){
+            rigthIndex = middleIndex-1
+
+        }
+        
+    }
+
+    else{
+        return null
+    }
+
+ }
+ //Write a function that takes in an array of strings and returns an array with every element
+//turned into a number
+//let nums = [“10”,”24”,”45”,”56”,”67”]
+
+
+
+
+
